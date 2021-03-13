7aab364df3d780f3b749eb7d77d319b5370ed4c3
emersonpessoa01 <eps011108@gmail.com>
2021-03-12 22:00:36 -0300

    upload all

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Slider from "@react-native-community/slider";
+import Clipboard from "expo-clipboard"
+
+let charset =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'!@#$%¨&*()_+<>";
 
 export default function App() {
+  const [password, setPassword] = useState("");
+  const [size, setSize] = useState(5);
+
+  const generatePass = () => {
+    // alert("Clicou no botão")
+    // setPassword("sujeitoProgramador")
+    // alert(size)
+    let pass = "";
+
+    for (let i = 0, n = charset.length; i < size; i++) {
+      pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    setPassword(pass);
+  };
+
+  const copyPress=()=>{
+    // alert("TESTAR")
+    Clipboard.setString(password)
+    alert("Senha copiada com sucesso!")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./src/assets/logo.png")} style={styles.logo} />
+      <Text style={styles.title}>{size} Caracteres</Text>
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="#ff0000"
+          maximumTrackTintColor="#000"
+          value={size}
+          onValueChange={(valor) => setSize(valor.toFixed(0))}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={generatePass}>
+        <Text style={styles.textButton}>Gerar senha</Text>
+      </TouchableOpacity>
+
+      {password !== "" && (
+        <View style={styles.area}>
+          <Text style={styles.password} onLongPress={copyPress}>{password}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -14,8 +61,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f3f3ff",
+  },
+  logo: {
+    marginBottom: 60,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  area: {
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: "#fff",
+    width: "80%",
+    borderRadius: 15,
+  },
+  button: {
+    backgroundColor: "#ffa200",
+    width: "80%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 7,
+    marginBottom: 25,
+  },
+  textButton: {
+    fontSize: 20,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  password: {
+    padding: 10,
+    textAlign: "center",
+    fontSize: 20,
   },
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+### Install:
+* npm install --global expo-cli
+* expo init <name_app>
+* expo install @react-native-community/slider
+* expo install expo-clipboard
